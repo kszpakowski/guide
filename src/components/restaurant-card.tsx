389f34d92e9099c9fafd3621c439d9b6428be66b
Karol Szpakowski <karol.szpakowski@gmail.com>
2025-03-31 02:31:22 +0200

    feat: display distance in restaurant card if available

--- src/components/restaurant-card.tsx
+++ src/components/restaurant-card.tsx
@@ -12,23 +12,27 @@ interface RestaurantCardProps {
 
 
 export const RestaurantCard = (props: RestaurantCardProps) => {
-    const { name, address, review, tags } = props.restaurant;
+    const { name, address, review, tags, distance } = props.restaurant;
 
     return (
         <div className="bg-stone-100 dark:bg-stone-900 shadow-md rounded-lg p-4">
             <h2 className="text-lg font-bold">{name}</h2>
             <p className="text-sm">{address}</p>
             <p className="text-base">{review}</p>
-            {tags.map(tag =>{
+            {tags.map(tag => {
                 return (
                     <span key={tag} className="inline-block bg-blue-200 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
                         {tag}
                     </span>
                 )
             })}
-            <p className="text-sm text-gray-500">
-                {props.restaurant.distance > 0 ? `Distance: ${props.restaurant.distance.toFixed(2)} km` : "Distance: Unknown"}
-            </p>
+            {
+                distance > 0 && (
+                    <p className="text-sm text-gray-500">
+                        Distance: {distance.toFixed(2)} km
+                    </p>
+                )
+            }
         </div>
     );
 }
